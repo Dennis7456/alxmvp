f8c438945521684c4dd4504becaf51c210cc5d92
Michael Ghalagher <lively222@live.com>
2022-06-07 00:06:10 +0300

    small changes

--- hirehub/config.py
+++ hirehub/config.py
@@ -1,12 +1,11 @@
 import os
-from dotenv import load_dotenv
-from pathlib import Path
+# from dotenv import load_dotenv
+# from pathlib import Path
 
-env_path = Path('../') / '.env'
-load_dotenv(dotenv_path=env_path)
+# env_path = Path('../') / '.env'
+# load_dotenv(dotenv_path=env_path)
 class Config:
     #Env
-    FLASK_APP = 'run.app'
     SECRET_KEY = os.getenv('SECRET_KEY')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     uri = os.getenv('DATABASE_URL')
