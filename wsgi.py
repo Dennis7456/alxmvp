9ea85080d4d952d8b995169669256f18b39a128d
Michael Ghalagher <lively222@live.com>
2022-06-06 20:33:10 +0300

    added procfile

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,5 @@ from hirehub import db
 from hirehub.models import User
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5005)
+# if __name__ == '__main__':
+#     app.run(debug=False, host='0.0.0.0', port=5005)
