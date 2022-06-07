78de6880163811776d05ab242b31bdf55cabd11e
webmasters2019 <lively222@live.com>
2022-06-07 12:36:13 +0300

    testing env

--- hirehub/config.py
+++ hirehub/config.py
@@ -19,4 +19,4 @@ from os import environ
 
 
 SECRET_KEY = environ.get('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
